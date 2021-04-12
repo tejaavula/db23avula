01c0db1c8e58fa513c68ec6720373db369da6433
tejaswi <avula.usedu1@gmail.com>
2021-04-12 10:12:12 -0500

    deleted

--- controllers/handbags.js
+++ controllers/handbags.js
@@ -126,5 +126,17 @@ exports.handbags_update_Page =  async function(req, res) {
         res.send(`{'error': '${err}'}`);
     }
 };
+// Handle a delete one view with id from query
+exports.handbags_delete_Page = async function(req, res) {
+    console.log("Delete view for id "  + req.query.id)
+    try{
+        result = await Handbags.findById(req.query.id)
+        res.render('handbagsdelete', { title: 'handbags Delete', toShow: result });
+    }
+    catch(err){
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
 
 
--- routes/handbags.js
+++ routes/handbags.js
@@ -14,3 +14,6 @@ router.get('/create', handbags_controlers.handbags_create_Page);
 
 /* GET create update page */
 router.get('/update', handbags_controlers.handbags_update_Page);
+
+/* GET create handbags page */
+router.get('/delete', handbags_controlers.handbags_delete_Page);
