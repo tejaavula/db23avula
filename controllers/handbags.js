0bad81e456344979b22b6b7741b2a4b994e04ff3
tejaswi <avula.usedu1@gmail.com>
2021-04-12 07:31:06 -0500

    added detail

--- controllers/handbags.js
+++ controllers/handbags.js
@@ -87,3 +87,18 @@ exports.handbags_view_all_Page = async function(req, res) {
     res.error(500,`{"error": ${err}}`);
     }
     };
+// Handle a show one view with id specified by query
+exports.handbags_view_one_Page = async function(req, res) {
+    console.log("single view for id "  + req.query.id)
+    try{
+        result = await Handbags.findById( req.query.id)
+        res.render('handbagsdetail', 
+{ title: 'handbags Detail', toShow: result });
+    }
+    catch(err){
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+ 
+
--- routes/handbags.js
+++ routes/handbags.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express.Router();
 const handbags_controlers= require('../controllers/handbags');
 
-/* GET handbagss */
+/* GET costumes */
 router.get('/', handbags_controlers.handbags_view_all_Page );
 module.exports = router;
+
+/* GET detail handbags page */
+router.get('/detail', handbags_controlers.handbags_view_one_Page);
